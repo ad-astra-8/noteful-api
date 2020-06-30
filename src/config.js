ff6b1afc72ec55aa2b5b748ae4f5eabbcfcc2b21
Wanda Antoine <wanda@thinkful.com>
2020-06-30 11:57:57 -0400

    updated package json l.11 congfig js l.8 and postggrator config l.9

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
   TEST_DATABASE_URL:
 		process.env.TEST_DATABASE_URL || 'postgresql://noteful@localhost/note-test',
 	DATABASE_URL:
-		process.env.DATABASE_URL || 'postgres://kvbsxavlswacnt:938fcfef2d19802a6a7894031e5264d9b2fbf3b99331678d265ac70a90574142@ec2-34-200-101-236.compute-1.amazonaws.com:5432/d69blgcd6o59jn'
+		process.env.DATABASE_URL || 'postgres://kvbsxavlswacnt:938fcfef2d19802a6a7894031e5264d9b2fbf3b99331678d265ac70a90574142@ec2-34-200-101-236.compute-1.amazonaws.com:5432/d69blgcd6o59jn?ssl=true'
 }
